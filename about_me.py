c233ce6b0a50074d9be6df3116aa3ddbd9443d57
Programming for Biology <pfb@info04.cshl.edu>
2025-10-14 11:14:32 -0400

    modified

--- about_me.py
+++ about_me.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 name = "Calvin"
+color = "Blue"
+activity = "Coding"
+animal = "Turtle"
 
-print("My name:", name)
-
+print(f"My name: {name}")
+print("My favorite color:", color)
+print("My favorite activity:", activity)
+print("My favorite animal:", animal)
 
